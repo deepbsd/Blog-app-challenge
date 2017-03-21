e6ca46273f7bf0973b39ab264f775810a0f704a5
David S. Jackson <deepbsd@yahoo.com>
2017-03-21 15:30:23 -0400

    trying to get GET check to work

--- server.js
+++ server.js
@@ -15,6 +15,34 @@ app.use('/', blogPostRouter);
 
 app.use('/blog-posts', blogPostRouter);
 
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+
+module.exports = {app, runServer, closeServer};
---
+++ test/test-server.js
@@ -0,0 +1,49 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+let server;
+
+describe('BlogPosts', function() {
+  // Before our tests run, we activate the server. Our `runServer`
+  // function returns a promise, and we return the promise by
+  // doing `return runServer`. If we didn't return a promise here,
+  // there's a possibility of a race condition where our tests start
+  // running before our server has started.
+  before(function() {
+    return runServer();
+  });
+
+  // Close server after these tests run in case
+  // we have other test modules that need to 
+  // call `runServer`. If server is already running,
+  // `runServer` will error out.
+  after(function() {
+    return closeServer();
+  });
+  // `chai.request.get` is an asynchronous operation. When
+  // using Mocha with async operations, we need to either
+  // return an ES6 promise or else pass a `done` callback to the
+  // test that we call at the end. We prefer the first approach, so
+  // we just return the chained `chai.request.get` object.
+  it('should list blog posts on GET', function() {
+    return chai.request(server)
+      .get('/blog-posts')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.above(0);
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.have.all.keys(
+            'title', 'author', 'content', 'publishDate');
+        });
+      });
+  });
+});
